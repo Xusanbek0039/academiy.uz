c368af5e14df323b637ac1c21489c5356002ee76
Snouden0071 <144625437+bekzodsindorov1524@users.noreply.github.com>
2023-10-28 23:28:21 +0500

    acv

--- django_school_management/students/tasks.py
+++ django_school_management/students/tasks.py
@@ -1,23 +1,23 @@
-from celery import shared_task
-from celery.utils.log import get_logger
+# from celery import shared_task
+# from celery.utils.log import get_logger
 
-from django.core.mail import send_mail
-from config.settings import EMAIL_HOST_USER
+# from django.core.mail import send_mail
+# from config.settings import EMAIL_HOST_USER
 
-from .models import AdmissionStudent
+# from .models import AdmissionStudent
 
-logger = get_logger(__name__)
+# logger = get_logger(__name__)
 
 
-@shared_task(name='send_admission_confirmation_email')
-def send_admission_confirmation_email(student_id):
-    student = AdmissionStudent.objects.get(id=student_id)
-    name = student.name
-    choosen_dept = student.choosen_department
-    send_mail(
-        f'Django-School-Management: Admission confirmed for student {name}',
-        f'Choosen Dept: {choosen_dept}',
-        EMAIL_HOST_USER,
-        [student.email, ], 
-        fail_silently=False
-    )
+# @shared_task(name='send_admission_confirmation_email')
+# def send_admission_confirmation_email(student_id):
+#     student = AdmissionStudent.objects.get(id=student_id)
+#     name = student.name
+#     choosen_dept = student.choosen_department
+#     send_mail(
+#         f'Django-School-Management: Admission confirmed for student {name}',
+#         f'Choosen Dept: {choosen_dept}',
+#         EMAIL_HOST_USER,
+#         [student.email, ], 
+#         fail_silently=False
+#     )
